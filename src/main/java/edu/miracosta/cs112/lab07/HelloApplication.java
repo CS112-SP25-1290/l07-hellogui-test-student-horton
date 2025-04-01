04122fb868e40a9fc0876f8ee9bc2851d30e35d4
Kristopher Horton <khorton@miracosta.edu>
2025-04-01 13:54:43 -0700

    Completed PT3

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,7 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
@@ -26,9 +27,19 @@ public class HelloApplication extends Application  { //inheriting core functiona
         Scene scene = new Scene(layout);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+        Label label = new Label("Hi there!");
+
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setBottomAnchor(label, 5.0);
+        anchorPane.setLeftAnchor(label, 5.0);
+        anchorPane.getChildren().add(label);
+
+        layout.getChildren().add(anchorPane);
     }
 
-    // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+
 
     // TODO: follow steps 22-34 in README.md to create an event handler
 }
